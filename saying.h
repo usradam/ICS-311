caa9aa9d6ec26a9aef48fc48a3a13835ed3c0285
Adam G <147444558+usradam@users.noreply.github.com>
2024-06-12 11:35:02 -1000

    Add files via upload

---
+++ saying.h
@@ -0,0 +1,26 @@
+/*****************************************************************
+//
+//  NAME:        Adam Graham
+//
+//  Assignment:  2
+//
+//  CLASS:       ICS 311
+//
+//  INSTRUCTOR:  Brook Conner
+//
+//  DATE:        June 12, 2024
+//
+//  FILE:        saying.h
+//
+//  DESCRIPTION:
+//   Header file for the Hawaiian and English sayings.
+//
+****************************************************************/
+
+struct Saying
+{
+    char hawaiianSaying[256];
+    char englishTranslation[256];
+    char hawaiianExplanation[256];
+    char englishExplanation[256];
+};
